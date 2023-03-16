6871fa63cca36d925ac94c827429e6402820c9b1
Nelson <nelson.chong@kewmann.com>
2023-03-16 09:45:37 +0800

    nelson - update 3
    preparations for java conversion, changed dependency checks logic

--- java_functions.py
+++ java_functions.py
@@ -7,7 +7,7 @@
 # multiple maincolumn and 1 subcolumn 
 # multiple maincolumn and multiple subcolumn 
 
-def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
+def RC_SCPB(dbSchema, source_name, currentTableName, error_code_option, rule_code,
             mainTable, subTable, dependencyTable, 
             mainColumn, subColumn, dependencyColumn, 
             main_query, sub_query, dependency_query):
@@ -15,7 +15,7 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
     error_code_column = "_dq_error_code_mandatory" if (error_code_option == "mandatory") else "_dq_error_code_optional"
     
     # processsing the main queries
-    tempMainQuery = "";
+    tempMainQuery = ""
     if ("," in mainColumn):
         mainColumns = mainColumn.split(",")
         tempColumns = []
@@ -37,8 +37,8 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
         tempMainQuery = tempMainQuery.replace("main_column", mainColumn)
     
     # processing the sub queries (if there are any)
-    tempSubQuery = "";
-    if len(subTable) > 0:
+    tempSubQuery = ""
+    if len(subTable) > 0:    
         if ("," in subColumn):
             subColumns = subColumn.split(",")
             tempColumns = []
@@ -51,6 +51,7 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
             for i in range(len(subColumns)):
                 tempQuery.append(sub_query)
                 tempQuery[i] = tempQuery[i].replace("sub_column", subColumns[i])
+                tempQuery[i] = tempQuery[i].replace("sub_table", subColumns[i].split(".")[0])
             tempSubQuery = (" and ").join(tempQuery)
         else:
             temp_column = subColumn.split(".")
@@ -58,9 +59,10 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
             subColumn = ".".join(temp_column)
             tempSubQuery = sub_query
             tempSubQuery = tempSubQuery.replace("sub_column", subColumn)
+            tempSubQuery = tempSubQuery.replace("sub_table", subColumn.split(".")[0])
     
     # processing the dependency queries (if there are any)
-    tempDependencyQuery = "";
+    tempDependencyQuery = ""
     if len(dependencyTable) > 0:
         if ("," in dependencyColumn):
             dependencyColumns = dependencyColumn.split(",")
@@ -81,8 +83,13 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
             dependencyColumn = ".".join(temp_column)
             tempDependencyQuery = dependency_query
             tempDependencyQuery = tempDependencyQuery.replace("dependency_column", dependencyColumn)
+        tempDependencyQuery = tempDependencyQuery.replace("source_name", source_name)
+        if "," in mainColumn:
+            tempDependencyQuery = tempDependencyQuery.replace("main_column", mainColumn.split(",")[0])
+        else:
+            tempDependencyQuery = tempDependencyQuery.replace("main_column", mainColumn)
     
-    # drafting the conditon query
+    # drafting the condition query
     mainQuery = tempMainQuery
     subQuery = tempSubQuery
     dependencyQuery = tempDependencyQuery
@@ -95,7 +102,7 @@ def RC_SCPB(dbSchema, currentTableName, error_code_option, rule_code,
     updateQuery = "update "+dbSchema+"."+currentTableName+" set `"+error_code_column+"` ='"+rule_code+"' where "+conditionQuery
     return updateQuery
 
-def RC_MCPB(dbSchema, currentTableName, error_code_option, rule_code,
+def RC_MCPB(dbSchema, source_name, currentTableName, error_code_option, rule_code,
             mainTable, subTable, dependencyTable, 
             mainColumn, subColumn, dependencyColumn, 
             main_query, sub_query, dependency_query):
@@ -122,7 +129,7 @@ def RC_MCPB(dbSchema, currentTableName, error_code_option, rule_code,
         tempMainQuery = tempMainQuery.replace("main_column", mainColumn)
     
     # processing the sub queries (if there are any)
-    tempSubQuery = "";
+    tempSubQuery = ""
     if len(subTable) > 0:
         if ("," in subColumn):
             subColumns = subColumn.split(",")
@@ -134,26 +141,51 @@ def RC_MCPB(dbSchema, currentTableName, error_code_option, rule_code,
                 tempColumns.append(column)
             tempSubQuery = sub_query
             for i in range(len(subColumns)):
-                tempQuery = tempQuery.replace("sub_column"+str(i+1), subColumns[i])
-            tempSubQuery = (" and ").join(tempQuery)
+                tempSubQuery = tempSubQuery.replace("sub_column"+str(i+1), subColumns[i])
+                tempSubQuery = tempSubQuery.replace("sub_table", subColumns[i].split(".")[0])
         else:
             temp_column = subColumn.split(".")
             temp_column[-1] = "`"+temp_column[-1]+"`"
             subColumn = ".".join(temp_column)
             tempSubQuery = sub_query
             tempSubQuery = tempSubQuery.replace("sub_column", subColumn)
+            tempSubQuery = tempSubQuery.replace("sub_table", subColumn.split(".")[0])
+    print("tempSubQuery",tempSubQuery)
     
+    # processing the dependency queries (if there are any)
+    tempDependencyQuery = ""
+    if len(dependencyTable) > 0:
+        if ("," in dependencyColumn):
+            dependencyColumns = dependencyColumn.split(",")
+            tempColumns = []
+            for column in dependencyColumns:
+                temp_column = column.split(".")
+                temp_column[-1] = "`"+temp_column[-1]+"`"
+                column = ".".join(temp_column)
+                tempColumns.append(column)
+            tempDependencyQuery = dependency_query
+            for i in range(len(dependencyColumns)):
+                tempDependencyQuery = tempDependencyQuery.replace("dependency_column"+str(i+1), dependencyColumns[i])
+                tempDependencyQuery = tempDependencyQuery.replace("dependency_table", dependencyColumns[i].split(".")[0])
+        else:
+            temp_column = dependencyColumn.split(".")
+            temp_column[-1] = "`"+temp_column[-1]+"`"
+            dependencyColumn = ".".join(temp_column)
+            tempDependencyQuery = dependency_query
+            tempDependencyQuery = tempDependencyQuery.replace("dependency_column", dependencyColumn)
+            tempDependencyQuery = tempDependencyQuery.replace("dependency_table", dependencyColumn.split(".")[0])
+    print("tempDependencyQuery",tempDependencyQuery)
     
     # drafting the conditon query
     mainQuery = tempMainQuery
     subQuery = tempSubQuery
+    dependencyQuery = tempDependencyQuery
     
-    conditionQuery = "("+mainQuery+")" if (subQuery != "") else mainQuery
+    conditionQuery = "("+mainQuery+")" if (subQuery != "" or dependencyQuery != "") else mainQuery
     if (len(subQuery) > 0): 
-        conditionQuery += " and ("+subQuery+")" if "," in subColumn else " and "+subQuery 
+        conditionQuery += " and ("+subQuery+")" if "," in subColumn else " and "+subQuery
+    if (len(dependencyQuery) > 0): 
+        conditionQuery += " and ("+dependencyQuery+")" if "," in dependencyColumn else " and "+dependencyQuery
 
     updateQuery = "update "+dbSchema+"."+currentTableName+" set `"+error_code_column+"` ='"+rule_code+"' where "+conditionQuery
     return updateQuery
-
-# def single_lookup_default():
-#     return output
